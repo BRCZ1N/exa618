b6b1132e25d6e6c5c259c55ed654566891d54bc7
Bruno <brunocamposo.r@gmail.com>
2026-03-13 14:29:25 -0300

    Parser

---
+++ atividade2/DomParser.py
@@ -0,0 +1,27 @@
+from xml.dom.minidom import parse
+
+mapDocument = parse('./atividade2/map.xml')
+#Formato CSV
+print("Starting DOM Parser...")
+isAmenity = False
+currentName = ""
+currentType = ""
+for currentNode in mapDocument.getElementsByTagName("node"):
+	tagNode = currentNode.getElementsByTagName("tag")
+	for tag in tagNode:
+		if tag.getAttribute("k") == "amenity":
+			isAmenity = True
+		if(isAmenity):
+			if tag.getAttribute("k") == "name":
+				currentName = tag.getAttribute("v")
+			elif tag.getAttribute("k") == "type":
+				currentType = tag.getAttribute("v")
+				print("type:", currentType)
+	if(isAmenity):
+		print("Latitude:", currentNode.getAttribute("lat"))	
+		print("Longitude:", currentNode.getAttribute("lon"))
+		print("Nome:", currentName)
+		print("Tipo:", currentType)
+	isAmenity = False
+
+     
