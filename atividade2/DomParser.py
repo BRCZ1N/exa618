e14bf9612cbc9461b61feeb64908f32dd0e8f3e9
Bruno <brunocamposo.r@gmail.com>
2026-03-14 11:50:13 -0300

    Update DomParser.py

--- atividade2/DomParser.py
+++ atividade2/DomParser.py
@@ -7,21 +7,32 @@ isAmenity = False
 currentName = ""
 currentType = ""
 for currentNode in mapDocument.getElementsByTagName("node"):
+    
 	tagNode = currentNode.getElementsByTagName("tag")
 	isAmenity = False
 	currentName = ""
 	currentType = ""
+ 
 	for tag in tagNode:
+     
 		if tag.getAttribute("k") == "amenity":
+      
 			currentType = tag.getAttribute("v")
 			isAmenity = True
+   
 		if(isAmenity):
+      
 			if tag.getAttribute("k") == "name":
+       
 				currentName = tag.getAttribute("v")
+    
 			elif tag.getAttribute("k") == "type":
+       
 				currentType = tag.getAttribute("v")
 				print("type:", currentType)
+    
 	if(isAmenity):
+     
 		print("Latitude:", currentNode.getAttribute("lat"))	
 		print("Longitude:", currentNode.getAttribute("lon"))
 		print("Nome:", currentName)
