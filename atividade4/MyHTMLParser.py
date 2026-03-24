4bba3f3376f28d9e17dcb221b8207667751e05a0
Bruno <brunocamposo.r@gmail.com>
2026-03-24 18:03:28 -0300

    Atividade 4

---
+++ atividade4/MyHTMLParser.py
@@ -0,0 +1,66 @@
+from html.parser import HTMLParser
+import urllib.request
+
+class MyHTMLParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.currentData = ""
+        self.title = ""
+        self.image = ""
+    def handle_starttag(self, tag, attrs): 
+        self.currentData = ""
+        if tag =="img":
+            for k, v in attrs:
+                if k == "src":
+                    self.image = v
+    def handle_endtag(self, tag):
+        if tag =="title":
+            self.title = self.currentData
+    def handle_data(self, data):
+        self.currentData += data
+    
+listURLS = []    
+with open("seeds.txt", "r", encoding="utf-8") as file:
+    for linha in file:
+        url = linha.strip()
+        if url:
+            listURLS.append(url)
+            
+pages = []
+for url in listURLS:
+    
+    page = urllib.request.urlopen(url)
+    parser = MyHTMLParser()
+    parser.feed(str(page.read().decode('utf-8')))
+    print("Título:", parser.title)
+    print("Imagem:", parser.image)
+    pages.append((parser.title,parser.image))
+    
+    with open("agregador.html", "w", encoding="utf-8") as file:
+
+        file.write("""<!DOCTYPE html>
+        <html>
+            <head>
+                <title>Lista de Alunos</title>
+            </head>
+            <body>
+                <h1>Lista de Alunos</h1>
+            <ul>
+        """)
+
+        for title,image in pages:
+
+            file.write(f"""
+            <li>
+                <h2>{title}</h2>
+                <img src="{image}" width="200">
+            </li>
+            """)
+
+        file.write("""
+             </ul>
+            </body>
+        </html>
+            """)
+    
+    
