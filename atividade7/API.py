16ded964ad6ec5de760d73b76e87a6eb65fcb83a
Bruno <brunocamposo.r@gmail.com>
2026-04-14 22:31:35 -0300

    Ajustando a conexão com a API

--- atividade7/API.py
+++ atividade7/API.py
@@ -51,6 +51,6 @@ def get_messages():
 
     return jsonify(mensagens), 200
 
-
 if __name__ == "__main__":
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
